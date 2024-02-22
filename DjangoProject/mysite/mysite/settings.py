f1bd2dc09f1ab239fc238fb11dedb9ce5b1019c7
joaofernandesLORD <jf.lipeto@gmail.com>
2024-02-21 23:25:04 -0400

    Primeiro projeto no django

--- DjangoProject/mysite/mysite/settings.py
+++ DjangoProject/mysite/mysite/settings.py
@@ -106,9 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'Americas/Sao_Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
